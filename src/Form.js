dd1aae83fcda20a86eb6da9ffeb48512b17ed9a7
jasonleewilson <info@jasonleewilson.com>
2021-08-17 17:46:14 -0500

    update ADA

--- src/Form.js
+++ src/Form.js
@@ -4,9 +4,9 @@ export default function Form() {
   return (
     <form name="contact" method="post">
       <input type="hidden" name="form-name" value="contact" />
-      <input className="d-block form-control" type="text" id="name" name="name" placeholder="Name" required />
-      <input className="d-block form-control" type="email" id="email" name="email" placeholder="Email" required />
-      <textarea className="d-block form-control" id="message" name="message" placeholder="Message" required />
+      <input className="d-block form-control" type="text" id="name" name="name" aria-label="name" placeholder="Name" required />
+      <input className="d-block form-control" type="email" id="email" name="email" aria-label="email" placeholder="Email" required />
+      <textarea className="d-block form-control" id="message" name="message" aria-label="message" placeholder="Message" required />
       <button className="w-100 btn btn-lg btn-primary" type="submit" value="Submit Message">Say Hi!!!</button>
     </form>
   );
