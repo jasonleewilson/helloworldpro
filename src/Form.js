17c8d9ec22932354cf11549e0408ea30dff05f4d
jasonleewilson <info@jasonleewilson.com>
2021-08-17 15:30:55 -0500

    form

--- src/Form.js
+++ src/Form.js
@@ -4,27 +4,10 @@ export default function Form() {
   return (
     <form name="contact" method="post">
       <input type="hidden" name="form-name" value="contact" />
-      <p>
-        <input className="d-block form-control" type="text" id="name" name="name" placeholder="Name" required />
-      </p>
-      <p>
-        <input className="d-block form-control" type="email" id="email" name="email" placeholder="Email" required />
-      </p>
-      <p>
-        <textarea className="d-block form-control" id="message" name="message" placeholder="Message" required />
-      </p>
-      <p>
-        <button className="w-100 btn btn-lg btn-primary" type="submit" value="Submit Message">Say Hi!!!</button>
-      </p>
+      <input className="d-block form-control" type="text" id="name" name="name" placeholder="Name" required />
+      <input className="d-block form-control" type="email" id="email" name="email" placeholder="Email" required />
+      <textarea className="d-block form-control" id="message" name="message" placeholder="Message" required />
+      <button className="w-100 btn btn-lg btn-primary" type="submit" value="Submit Message">Say Hi!!!</button>
     </form>
   );
 }
-
-
-<form name="contact" method="POST" netlify netlify-honeypt="bot-field">
-            <input className="d-block form-control" type="text" name="name" placeholder="Name" /><br />
-            <input className="d-block form-control" type="email" name="email" placeholder="Email" /><br />
-            <textarea className="d-block form-control" name="message" placeholder="Message" /><br />
-            <input type="hidden" name="form-name" value="contact" />
-            <div data-netlify-recaptcha="true"></div>
-          </form>
