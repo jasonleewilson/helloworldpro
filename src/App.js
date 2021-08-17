4688fd67b3b13846efe1e16776bea1a46e45c586
jasonleewilson <info@jasonleewilson.com>
2021-08-17 13:33:40 -0500

    form

--- src/App.js
+++ src/App.js
@@ -7,17 +7,27 @@ function App() {
         <h1>
           HelloWorldPro.com
         </h1>
-        <footer className="App-footer">
-        Coded by: &nbsp;
-        <a
-          className="App-link"
-          href="https://jasonleewilson.com"
-          target="_blank"
-          rel="noopener noreferrer">
-            jasonleewilson.com
-        </a>
-      </footer>
       </header>
+      <div className="container">
+        <div className="myform">
+          <form action="POST" data-netlify="true">
+            <input type="text" name="name" id="name" placeholder="Name" />
+            <input type="text" name="email" id="email" placeholder="Email" />
+            <textarea name="message" id="message" placeholder="Message" />
+            <input type="submit" value="Send Message" />
+          </form>
+        </div>
+      </div>
+      <footer className="App-footer">
+          Coded by: &nbsp;
+          <a
+            className="App-link"
+            href="https://jasonleewilson.com"
+            target="_blank"
+            rel="noopener noreferrer">
+              jasonleewilson.com
+          </a>
+        </footer>
     </div>
   );
 }
