8b34ba85b26944717c2f9d2787873d3593d88830
Jason Lee Wilson <info@jasonleewilson.com>
2024-02-22 18:46:37 -0600

    updated babel package

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import './App.css';
+import "./App.css";
 import Form from "./Form";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          HelloWorldPro.com
-        </h1>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>HelloWorldPro.com</h1>
       </header>
-      <div className="container">
-        <div className="myform">
+      <div className='container'>
+        <div className='myform'>
           <Form />
         </div>
       </div>
-      <footer className="App-footer">
-          <a
-            className="App-link"
-            href="https://github.com/jasonleewilson/helloworldpro"
-            target="_blank"
-            rel="noopener noreferrer">
-              View the code @ gitHub
-          </a>
-        </footer>
+      <footer className='App-footer'>
+        <a
+          className='App-link'
+          href='https://github.com/jasonleewilson/helloworldpro'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          View the code @ gitHub
+        </a>
+      </footer>
     </div>
   );
 }
