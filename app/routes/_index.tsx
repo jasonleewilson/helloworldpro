850a028f031184c4b1170c07877acef9f0134035
Jason Wilson <info@jasonleewilson.com>
2025-03-11 20:44:29 -0500

    added branches

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -17,7 +17,7 @@ export default function Index() {
       {/* Plugins:
     - @tailwindcss/forms */}
 
-      <section className='relative flex flex-wrap lg:h-screen lg:items-center'>
+      <section className='relative flex flex-wrap h-screen items-center'>
         <div className='w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24'>
           <div className='mx-auto max-w-lg text-center'>
             <h1 className='text-gray-700 text-2xl font-bold sm:text-3xl'>
