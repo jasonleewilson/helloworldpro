31a07bdbd9a078c52a3bc4e5bdecf1428d471f68
Jason Lee Wilson <robotici@yahoo.com>
2025-03-12 21:12:31 -0500

    added GTM

--- app/root.tsx
+++ app/root.tsx
@@ -30,8 +30,37 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
+        {/* Google Analytics Script */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=ID`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5796J69');<
+          `,
+          }}
+        />
       </head>
       <body>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            title='GTM'
+            src='https://www.googletagmanager.com/ns.html?id=GTM-5796J69'
+            height='0'
+            width='0'
+            className='hidden invisible'
+            // style='display:none;visibility:hidden'
+          ></iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
+
         {children}
         <ScrollRestoration />
         <Scripts />
