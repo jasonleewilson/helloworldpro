9258a0208f5bdbb9fb49db254de2f5227f4b13eb
Jason Lee Wilson <robotici@yahoo.com>
2025-03-26 14:37:20 -0500

    merged dev to master

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -17,7 +17,7 @@ export default function Index() {
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
         <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
-          <h1>About page. (coming from dev branch)</h1>
+          <h1>About page.</h1>
           <ContactForm />
         </div>
       </div>
