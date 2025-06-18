ec03c31bbfd15347ffaf91df6ac3ae175c73456f
Jason Lee Wilson <robotici@yahoo.com>
2025-06-17 22:26:39 -0500

    fixed test burger1.png path

--- app/components/Desktop2.tsx
+++ app/components/Desktop2.tsx
@@ -2,7 +2,7 @@
 
 const Desktop2 = () => (
   <>
-    <div className='h-96 my-8 flex content-end sm:flex-wrap md:flex-nowrap items-start gap-0 px-8 mx-8 bg-white bg-[url("app/assets/images/burger1.png")] bg-no-repeat bg-size-[auto_700px] bg-position-[center_right_-10rem] rounded-2xl'>
+    <div className='h-96 my-8 flex content-end sm:flex-wrap md:flex-nowrap items-start gap-0 px-8 mx-8 bg-white bg-[url("assets/images/burger1.png")] bg-no-repeat bg-size-[auto_700px] bg-position-[center_right_-10rem] rounded-2xl'>
       <div className='flex w-1/3 p-8 h-full items-center justify-center'>
         <div className='text-left'>
           <h1 className='py-4 bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-5xl font-extrabold text-transparent'>
@@ -34,7 +34,7 @@ const Desktop2 = () => (
           </a>
         </div>
       </div>
-      <div className='h-96 order-1 md:order-2 w-full md:w-2/3 bg-[url("app/assets/images/burger1.png")] bg-no-repeat bg-size-[auto_700px] bg-position-[center_center_-10rem] md:bg-position-[center_right_-10rem] rounded-none  md:rounded-2xl'></div>
+      <div className='h-96 order-1 md:order-2 w-full md:w-2/3 bg-[url("assets/images/burger1.png")] bg-no-repeat bg-size-[auto_700px] bg-position-[center_center_-10rem] md:bg-position-[center_right_-10rem] rounded-none  md:rounded-2xl'></div>
     </div>
   </>
 );
