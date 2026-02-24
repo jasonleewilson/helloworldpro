2651719f92eae1597abe3685a2e107022928d3f2
Jason Wilson <robotici@yahoo.com>
2026-02-23 21:06:24 -0600

    Replace dev branch with new Astro + Tailwind + React + shadcn project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
